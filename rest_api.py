16d5a5a564c3bfae62564796bb0891e788290cdb
sharvesh12 <sharvesh.gp@gmail.com>
2023-08-23 12:30:41 +0200

    API call combi successful

--- rest_api.py
+++ rest_api.py
@@ -1,3 +1,4 @@
+#should figure out h
 import requests
 import pandas as pd
 import json
@@ -11,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 class Api:
         def make_flight_api_request(self,url,headers,params):
-         response = requests.post(url, headers, params)
+         response = requests.get(url, headers, params)
          if response.status_code == 200:
              data = response.json()
              print(f"API response: {data}") 
