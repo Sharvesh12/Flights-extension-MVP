6431c096801e90c22aa8ca1d9887e11c27bc9beb
sharvesh12 <sharvesh.gp@gmail.com>
2023-10-01 12:30:57 +0200

    DB Manager change

--- db_manager.py
+++ db_manager.py
@@ -21,7 +21,7 @@ class DBManager:
         database=credentials['database']
         username=credentials['username']
         password=quote_plus(credentials['password'])
-        engine=create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database}')
+        engine=create_engine(url=f'postgresql://{username}:{password}@{host}:{port}/{database}',schema='public')
         logger.info('CONNECTED')        
 
     def run_query(self, query_file_name:str, params=None) -> pd.DataFrame:
