c97ef9e8fa36f8410d6fcdbe088ee46099de3207
sharvesh12 <sharvesh.gp@gmail.com>
2023-10-01 12:26:11 +0200

    changes to DB manager

--- db_manager.py
+++ db_manager.py
@@ -21,7 +21,7 @@ class DBManager:
         database=credentials['database']
         username=credentials['username']
         password=quote_plus(credentials['password'])
-        self.connection=create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database}')
+        engine=create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database}')
         logger.info('CONNECTED')        
 
     def run_query(self, query_file_name:str, params=None) -> pd.DataFrame:
@@ -53,8 +53,8 @@ class DBManager:
 
     def create_table_from_df(self, df:pd.DataFrame, table_name:str) -> None:
         '''
-        :param query_file_name: pandas datagrame supposed to be written to DB table
+        :param query_file_name: pandas dataframe supposed to be written to DB table
         :return: void
         '''
-        df.to_sql(table_name,con=self.connection, index=False, if_exists='append')
+        df.to_sql(table_name,con=engine, index=False, if_exists='append')
         logger.info(f'Dumped to DB')
