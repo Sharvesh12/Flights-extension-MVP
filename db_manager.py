5e521fb6b1ec47c8da9fd1991cedd33158d5d6fc
sharvesh12 <sharvesh.gp@gmail.com>
2023-10-02 21:30:32 +0200

    pscopg2 added

--- db_manager.py
+++ db_manager.py
@@ -1,62 +1,44 @@
 import config
-import sqlalchemy
-from sqlalchemy.pool import NullPool
-from sqlalchemy.engine import url as sa_url
-from sqlalchemy import MetaData
 from config import logging
 import pandas as pd
 import os
 from datetime import date
 from dateutil.relativedelta import relativedelta
 from urllib.parse import quote_plus
-from sqlalchemy import create_engine
 logger = logging.getLogger(__name__)
+import psycopg2
+import pandas as pd
 
 class DBManager:
-    def __init__(self, credentials: dict):
-        '''
-        :param credentials: dictionary holding username and password for connection
-        '''
-        port=credentials['port']
-        host=credentials['host']
-        database=credentials['database']
-        username=credentials['username']
-        password=quote_plus(credentials['password'])
-        metadata = MetaData(schema="public")
-        self.connection=create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database}', metadata=metadata)
-        logger.info('CONNECTED')        
 
-    def run_query(self, query_file_name:str, params=None) -> pd.DataFrame:
-        '''
-        :param query_file_name: name of the file in same directory that holds PG SQL text
-        :return: pandas DataFrame
-        '''
-        with open(f'{config.working_dir}/queries/{query_file_name}.sql','r') as query_file:
-            query_text=query_file.read()
-            
-            logger.info(f'Running Query {query_file_name}')
-            df=pd.read_sql_query(sql=query_text, con=self.connection)
-            logger.info(f'Execution Completed')
-        return df
-
-
-    def run_etl_query(self, query_file_name:str)->None:
-        '''
-        :param query_file_name: name of the file in same directory that holds PG SQL DDL
-        :return: None
-        '''
-        with open(f'{config.working_dir}/queries/{query_file_name}.sql','r') as query_file:
-            query_text=query_file.read()
-            logger.info(f'Running DDL Query {query_file_name}')
-            self.connection.execute(query_text)
-        logger.info(f'Execution Completed')
+    def __init__(self, credentials):
         
-
-
-    def create_table_from_df(self, df:pd.DataFrame, table_name:str) -> None:
-        '''
-        :param query_file_name: pandas dataframe supposed to be written to DB table
-        :return: void
-        '''
-        df.to_sql(name=table_name,con=self.connection, index=False, if_exists='append')
-        logger.info(f'Dumped to DB')
+        self.connection = psycopg2.connect(
+            host=credentials['host'],
+            database=credentials['database'], 
+            user=credentials['username'],
+            password=credentials['password']
+        )
+        
+        print('Connected!')
+        
+    def run_query(self, query):
+        
+        df = pd.read_sql(query, self.connection)
+        
+        return df
+    
+    def run_ddl_query(self, query):
+        
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        self.connection.commit()
+        
+        print('Query executed!')
+        
+    def create_table_from_df(self, df, table_name):
+      
+        df.to_sql(name=table_name, con=self.connection, 
+                  index=False, if_exists='append')
+      
+        print('Table created!')
