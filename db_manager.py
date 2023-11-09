74af077397251199f003ade15697a1a5935aafbe
sharvesh12 <sharvesh.gp@gmail.com>
2023-11-09 13:23:12 +0100

    Holiday flights and DB manager module changes

--- db_manager.py
+++ db_manager.py
@@ -1,5 +1,6 @@
 import config
 import sqlalchemy
+from sqlalchemy import text
 from sqlalchemy.pool import NullPool
 from sqlalchemy.engine import url as sa_url
 from sqlalchemy.exc import OperationalError
@@ -52,16 +53,31 @@ class DBManager:
         return df
 
 
-    def run_etl_query(self, query_file_name:str)->None:
+    def run_etl_query(self, query_file_name: str) -> None:
         '''
-        :param query_file_name: name of the file in same directory that holds PG SQL DDL
+        :param query_file_name: name of the file in the same directory that holds PG SQL DDL
         :return: None
         '''
-        with open(f'{config.working_dir}/queries/{query_file_name}.sql','r') as query_file:
-            query_text=query_file.read()
-            logger.info(f'Running DDL Query {query_file_name}')
-            self.connection.execute(query_text)
-        logger.info(f'Execution Completed')
+        session = None  # Initialize session outside the try block
+        try:
+            Session = sessionmaker(bind=self.connection)
+            session = Session()
+
+            with open(f'{config.working_dir}/queries/{query_file_name}.sql', 'r') as query_file:
+                query_text = query_file.read()
+                logger.info(f'Running DDL Query {query_file_name}')
+                # Explicitly declare the SQL expression as text
+                sql_expression = text(query_text)
+                session.execute(sql_expression)
+
+            session.commit()
+            logger.info(f'Execution Completed')
+        except OperationalError as e:
+            logger.error(f"OperationalError: {e}")
+            # Handle the OperationalError - you can choose to retry the operation or take appropriate action
+            raise
+        finally:
+            session.close()
         
 
 
@@ -73,3 +89,10 @@ class DBManager:
         df.to_sql(name=table_name,con=self.connection, index=False, if_exists='append')
         logger.info(f'Dumped to DB')
 
+    def create_table_from_df_replace(self, df:pd.DataFrame, table_name:str) -> None:
+        '''
+        :param query_file_name: pandas dataframe supposed to be written to DB table
+        :return: void
+        '''
+        df.to_sql(name=table_name,con=self.connection, index=False, if_exists='replace')
+        logger.info(f'Dumped to DB')
